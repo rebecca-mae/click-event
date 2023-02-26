647917d56a143f21ad0bf9aad26f2d2dae693519
rebecca_mae <rebecca.small18@hotmail.com>
2023-02-26 14:34:58 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var button = document.querySelector(".show-cat");
+
+button.addEventListener("click", function() {
+    cat.classList.add("show");
+}
+)
+
+var cat = document.querySelector(".cat");
+
